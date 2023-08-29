d42ae5475921fb89cefeb98789310db353efbe35
Liam Howell <liam@liam.codes>
2023-08-29 13:34:26 +0100

    feat: rewrite compose function to reduce bundled size

--- packages/tailwind-compose/src/core/compose.ts
+++ packages/tailwind-compose/src/core/compose.ts
@@ -2,51 +2,30 @@ import { construct } from '@src/lib/construct';
 import type { ComposeFactory, ComposerFn, Props, Target } from '@types';
 
 const Compose = <P extends Props>(target: Target<P>, classes: ComposerFn<P>) => {
-  const compose = construct<P, Record<string, unknown>>({
-    classes,
-    target,
-  });
-
-  Object.defineProperty(compose, 'attrs', {
-    value: <A extends Props>(attrs: A) => {
-      return construct<P, A>({
-        attrs,
-        classes: classes as ComposerFn<P & A>,
-        target: target as Target<P>,
-      });
+  return Object.assign(construct<P, Record<string, unknown>>({ classes, target }), {
+    attrs: <A extends Props>(attrs: A) => {
+      return construct<P, A>({ attrs, classes: classes as ComposerFn<P & A>, target });
     },
   });
-
-  return compose;
 };
 
-const ComposeFactoryProxyInstance = new Proxy(Compose, {
+export const compose = new Proxy(Compose, {
   get(target: typeof Compose, property: string) {
     if (Reflect.has(target, property)) {
       return Reflect.get(target, property);
     }
 
-    const ComposeTag = <P extends Props>(classes: ComposerFn<P>) => {
-      return construct<P, Record<string, unknown>>({
-        classes,
-        target: property as Target<P>,
-      });
-    };
-
-    Object.defineProperty(ComposeTag, 'attrs', {
-      value: <A extends Props>(attrs: A) => {
-        return <P extends Props>(classes: ComposerFn<P & A>) => {
-          return construct<P, A>({
-            attrs,
-            classes,
-            target: property as Target<P>,
-          });
-        };
+    return Object.assign(
+      <P extends Props>(classes: ComposerFn<P>) => {
+        return construct<P, Record<string, unknown>>({ classes, target: property as Target<P> });
       },
-    });
-
-    return ComposeTag;
+      {
+        attrs: <A extends Props>(attrs: A) => {
+          return <P extends Props>(classes: ComposerFn<P & A>) => {
+            return construct<P, A>({ attrs, classes, target: property as Target<P> });
+          };
+        },
+      },
+    );
   },
-});
-
-export const compose = ComposeFactoryProxyInstance as unknown as ComposeFactory;
+}) as unknown as ComposeFactory;
